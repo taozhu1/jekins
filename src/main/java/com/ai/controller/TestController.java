05f1b4ba243a337279655c3cc61f2ea92e80d906
taozhu <taozhu@gmail.com>
2025-05-28 20:15:50 +0800

    init

---
+++ src/main/java/com/ai/controller/TestController.java
@@ -0,0 +1,21 @@
+package com.ai.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public String test() {
+        return "ok";
+    }
+
+}
